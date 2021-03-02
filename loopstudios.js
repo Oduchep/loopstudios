7182240398893c196cc1ae392f4dd5941314759d
Peter Okerulu <oduchep@gmail.com>
2021-03-02 21:12:04 +0100

    page complete

---
+++ loopstudios.js
@@ -0,0 +1,4 @@
+document.querySelector(".mobile-menu-icons").addEventListener("click", () => {
+	document.querySelector(".main-nav").classList.toggle("mobile-nav");
+	document.querySelector(".trial").classList.toggle("mobile-nav");
+});
